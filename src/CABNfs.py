bc00a1315809eeccc7244503654804be4935d7e0
Christian Badolato <badolato.c@gmail.com>
2020-10-16 17:16:11 -0400

    Fixing bugs preventing opening, updating some methods

--- src/CABNfs.py
+++ src/CABNfs.py
@@ -13,6 +13,7 @@ import os
 
 from fuse import FUSE, FuseOSError, Operations, LoggingMixIn
 
+
 class CABNfs(LoggingMixIn, Operations):
 
     def __init__(self, root, replication_factor):
@@ -20,8 +21,10 @@ class CABNfs(LoggingMixIn, Operations):
         self.root = realpath(root)
         self.rwlock = Lock()
 
-    def __call__(self, op, path, *args):
-        return super(CABNfs, self).__call__(op, self.root + path, *args)
+    def _real_path(self, path):
+        if path[0] == '/':
+            path = path[1:]
+        return os.path.join(self.root, path)
 
     # Functions which are not implemented
     getxattr = None
@@ -32,22 +35,42 @@ class CABNfs(LoggingMixIn, Operations):
     chown = None
     symlink = None
     readlink = None
+    link = None
 
-    # Functions which can be base functions
-    mknod = os.mknod
-    utimens = os.utime
+    # Functions which can be base functions with adjusted paths
+
+    def mknod(self, path, mode, dev):
+        return os.mknod(self._real_path(path), mode, dev)
+
+    def utimens(self, path, times=None):
+        return os.utime(self._real_path(path), times)
+
+    def getattr(self, path, fh=None):
+        st = os.lstat(self._real_path(path))
+        return dict((key, getattr(st, key)) for key in ('st_atime', 'st_ctime',
+            'st_gid', 'st_mode', 'st_mtime', 'st_nlink', 'st_size', 'st_uid',
+            'st_dev', "st_ino", "st_rdev", "st_blksize", "st_blocks"))
+
+    def statfs(self, path):
+        stv = os.statvfs(self._real_path(path))
+        return dict((key, getattr(stv, key)) for key in ('f_bavail', 'f_bfree',
+            'f_blocks', 'f_bsize', 'f_favail', 'f_ffree', 'f_files', 'f_flag',
+            'f_frsize', 'f_namemax'))
+
+    def readdir(self, path, fh):
+        return ['.', '..'] + os.listdir(self._real_path(path))
+
+    def access(self, path, mode):
+        if not os.access(self._real_path(path), mode):
+            raise FuseOSError(EACCES)
 
     # Functions with custom logic
 
-    def open(self, path, flags, mode):
-        return os.open(path, flags, mode)
-
-    def access(self, path, mode):
-        if not os.access(path, mode):
-            raise FuseOSError(EACCES)
+    def open(self, path, flags):
+        return os.open(self._real_path(path), flags)
 
     def create(self, path, mode):
-        return os.open(path, os.O_WRONLY | os.O_CREAT | os.O_TRUNC, 0o666)
+        return os.open(self._real_path(path), os.O_WRONLY | os.O_CREAT, mode)
 
     def flush(self, path, fh):
         return os.fsync(fh)
@@ -58,40 +81,23 @@ class CABNfs(LoggingMixIn, Operations):
         else:
           return os.fsync(fh)
 
-    def getattr(self, path, fh=None):
-        st = os.lstat(path)
-        return dict((key, getattr(st, key)) for key in ('st_atime', 'st_ctime',
-            'st_gid', 'st_mode', 'st_mtime', 'st_nlink', 'st_size', 'st_uid'))
-
-    def link(self, target, source):
-        return os.link(source, target)
-
     def read(self, path, size, offset, fh):
         with self.rwlock:
             os.lseek(fh, offset, 0)
             return os.read(fh, size)
 
-    def readdir(self, path, fh):
-        return ['.', '..'] + os.listdir(path)
-
     def release(self, path, fh):
         return os.close(fh)
 
     def rename(self, old, new):
-        return os.rename(old, self.root + new)
-
-    def statfs(self, path):
-        stv = os.statvfs(path)
-        return dict((key, getattr(stv, key)) for key in ('f_bavail', 'f_bfree',
-            'f_blocks', 'f_bsize', 'f_favail', 'f_ffree', 'f_files', 'f_flag',
-            'f_frsize', 'f_namemax'))
+        return os.rename(self._real_path(old), self._real_path(new))
 
     def truncate(self, path, length, fh=None):
-        with open(path, 'r+') as f:
+        with open(self._real_path(path), 'r+') as f:
             f.truncate(length)
 
     def unlink(self, path):
-        return os.unlink(path)
+        return os.unlink(self._real_path(path))
 
     def write(self, path, data, offset, fh):
         with self.rwlock:
