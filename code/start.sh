de34e30590ac4453bd33a1ff17e3307ed259f564
Christian Badolato <badolato.c@gmail.com>
2020-12-02 22:26:13 -0500

    Resetting defaults

--- code/start.sh
+++ code/start.sh
@@ -1,4 +1,3 @@
 rabbitmq-server -detached
 sleep 180
-#python3 src/CABNfs.py /filesystem /mountpoint 3
-python3 src/CABNfs.py /filesystem /mountpoint 2
+python3 src/CABNfs.py /filesystem /mountpoint 3
